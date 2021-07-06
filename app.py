2613b38a3aaf16aa1f45f310fb7f09434aa195b5
Max Fan <root@max.fan>
2021-07-06 11:32:00 -0400

    Add .tex rendering!

--- app.py
+++ app.py
@@ -12,19 +12,39 @@ templates = Jinja2Templates(directory="templates")
 with open("conf.yaml") as f:
     keywords = yaml.load(f)["keywords"]
 
+
 @app.get("/", response_class=PlainTextResponse)
 async def read_root() -> str:
-    return PlainTextResponse("Go to /{location} (for all submissions) or /{location}/{start-date}/{end-date} to start. E.g. <a href=\"/michigan\">/michigan</a> or <a href=\"/michigan/2021-5-1/2021-5-7\">/michigan/2021-5-1/2021-5-7</a>")
+    return PlainTextResponse(
+        'Go to /{location} (for all submissions) or /{location}/{start-date}/{end-date} to start. E.g. <a href="/michigan">/michigan</a> or <a href="/michigan/2021-5-1/2021-5-7">/michigan/2021-5-1/2021-5-7</a>'
+    )
 
-def nab_written_submission(location: str) -> pd.DataFrame:
+
+def nab_submissions(location: str, start=False, stop=False) -> pd.DataFrame:
     if location == "michigan":
-        submissions = pd.read_csv(f"https://o1siz7rw0c.execute-api.us-east-2.amazonaws.com/beta/submissions/csv/{location}")
+        submissions = pd.read_csv(
+            f"https://o1siz7rw0c.execute-api.us-east-2.amazonaws.com/beta/submissions/csv/{location}"
+        )
     else:
-        submissions = pd.read_csv(f"https://k61e3cz2ni.execute-api.us-east-2.amazonaws.com/prod/submissions/csv/{location}")
+        submissions = pd.read_csv(
+            f"https://k61e3cz2ni.execute-api.us-east-2.amazonaws.com/prod/submissions/csv/{location}"
+        )
 
-    submissions["datetime"] = submissions["datetime"].apply(lambda x: parser.parse(x.split("(")[0]))
+    submissions["datetime"] = submissions["datetime"].apply(
+        lambda x: parser.parse(x.split("(")[0])
+    )
 
+    if start and stop:
+        submissions = submissions[
+            submissions["datetime"].apply(lambda x: start < x and x < stop)
+        ]
+    return submissions
+
+
+def nab_written_submission(location: str, start=False, stop=False) -> pd.DataFrame:
+    submissions = nab_submissions(location, start, stop)
     return submissions[submissions["type"] == "written"].fillna("")
+
 
 @app.get("/{location}", response_class=HTMLResponse)
 async def classify(request: Request, location: str):
@@ -33,14 +53,19 @@ async def classify(request: Request, location: str):
 
     print(written_submissions.columns, written_submissions, written_submissions["type"])
     if len(written_submissions):
-        return templates.TemplateResponse("location.html",
-                                          {
-                                              "request": request,
-                                              "location": location,
-                                              "written_submissions": written_submissions
-                                          })
+        return templates.TemplateResponse(
+            "location.html",
+            {
+                "request": request,
+                "location": location,
+                "written_submissions": written_submissions,
+            },
+        )
     else:
-        raise HTTPException(status_code=404, detail=f"No written submissions found for {location}.")
+        raise HTTPException(
+            status_code=404, detail=f"No written submissions found for {location}."
+        )
+
 
 @app.get("/{location}/{start}/{stop}", response_class=HTMLResponse)
 async def classify_filter(request: Request, location: str, start: str, stop: str):
@@ -48,31 +73,90 @@ async def classify_filter(request: Request, location: str, start: str, stop: str
     start = parser.parse(start).replace(tzinfo=pytz.UTC)
     stop = parser.parse(stop).replace(tzinfo=pytz.UTC)
 
-    written_submissions = nab_written_submission(location)
-    written_submissions_filtered = written_submissions[written_submissions["datetime"].apply(lambda x: start < x and x < stop)]
+    written_submissions = nab_written_submission(location, start, stop)
 
-    print(written_submissions.columns, written_submissions_filtered, written_submissions["type"])
+    print(
+        written_submissions.columns,
+        written_submissions_filtered,
+        written_submissions["type"],
+    )
     if len(written_submissions_filtered):
-        return templates.TemplateResponse("location.html",
-                                          {
-                                              "request": request,
-                                              "location": location,
-                                              "written_submissions": written_submissions_filtered
-                                          })
+        return templates.TemplateResponse(
+            "location.html",
+            {
+                "request": request,
+                "location": location,
+                "written_submissions": written_submissions_filtered,
+            },
+        )
     else:
-        raise HTTPException(status_code=404, detail=f"No written submissions found for {location} between {start} and {stop}.")
+        raise HTTPException(
+            status_code=404,
+            detail=f"No written submissions found for {location} between {start} and {stop}.",
+        )
+
 
 @app.get("/{location}/submit", response_class=HTMLResponse)
 async def submit(request: Request, location: str):
-    written_submissions = nab_written_submission(location)
+    all_submissions = nab_submissions(location)
 
     classifications = dict(request.query_params)
+    data = []
 
-    theory = written_submissions[written_submissions["id"].apply(
-        lambda x: str(x) in classifications and classifications[str(x)] == "theory"
-    )]
-    coi = written_submissions[written_submissions["id"].apply(
-        lambda x: str(x) in classifications and classifications[str(x)] == "coi"
-    )]
+    for week_start, submissions in all_submissions.groupby(
+        pd.Grouper(key="datetime", freq="W", label="left")
+    ):
+        written_df = submissions[submissions["type"] == "written"].fillna("")
+        written = len(written_df)
+        written_theory = len(
+            submissions[
+                submissions["id"].apply(
+                    lambda x: str(x) in classifications
+                    and classifications[str(x)] == "theory"
+                )
+            ]
+        )
+        written_coi = len(
+            submissions[
+                submissions["id"].apply(
+                    lambda x: str(x) in classifications
+                    and classifications[str(x)] == "coi"
+                )
+            ]
+        )
+        written_comments = pd.to_numeric(written_df["numberOfComments"]).sum()
 
-    print(classifications, location, len(theory), len(coi), len(written_submissions))
+        districts_df = submissions[submissions["type"] == "written"].fillna("")
+
+        plan_df = submissions[submissions["type"] == "plan"].fillna("")
+        plan = len(plan_df)
+        plan_comments = pd.to_numeric(plan_df["numberOfComments"]).sum()
+        cd = len(submissions[submissions["districttype"] == "ush"])
+        sd = len(submissions[submissions["districttype"] == "ush"])
+        hd = len(submissions[submissions["districttype"] == "ush"])
+
+        coi_df = submissions[submissions["type"] == "coi"].fillna("")
+        coi = len(coi_df)
+        coi_comments = pd.to_numeric(coi_df["numberOfComments"]).sum()
+        data.append(
+            {
+                "week_start": week_start,
+                "written": written,
+                "written_theory": written_theory,
+                "written_coi": written_coi,
+                "written_comments": written_comments,
+                "plan": plan,
+                "plan_comments": plan_comments,
+                "cd": cd,
+                "sd": sd,
+                "hd": hd,
+                "coi": coi,
+                "coi_comments": coi_comments,
+            }
+        )
+
+    print(data)
+    return templates.TemplateResponse(
+        "render.html",
+        {"request": request, "location": location, "data": list(enumerate(data))},
+    )
